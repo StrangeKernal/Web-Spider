0011a882b5e3d05bfd72fce0fb2077352b959c35
strangeKernal <18252090618@163.com>
2019-10-12 16:35:52 +0800

    一念永恒小说爬虫

--- novel_spider.py
+++ novel_spider.py
@@ -1,28 +1,49 @@
-# -*- coding:UTF-8 -*-
-
+# -*- coding:utf-8 -*-
 from bs4 import BeautifulSoup
-import requests
+import sys, requests
+
+class Download(object):
+    def __init__(self):
+        self.server         = 'https://www.biqukan.com/'
+        self.target         = 'https://www.biqukan.com/1_1094/'
+        self.chapter_name   = []
+        self.chapter_url    = []
+        self.chapter_nums   = 0
+        pass
+    def get_novel(self):
+        req             = requests.get(url = self.target)
+        req.encoding    = 'gbk'
+        html            = req.text
+        bs_html         = BeautifulSoup(html, 'html.parser')
+        listmain        = BeautifulSoup(str(bs_html.find_all('div', class_='listmain')[0]), 'html.parser')
+        listmain_a      = listmain.find_all('a')
+
+        self.chapter_nums = len(listmain_a[13:-2])
+        for each in listmain_a[13:-2]:
+            sub_url = str(each.get('href')).lstrip('/')
+            self.chapter_name.append(each.string)
+            self.chapter_url.append(self.server + sub_url)
+
+    def get_text(self,target):
+        req             = requests.get(url = target)
+        req.encoding    = 'gbk'
+        html            = req.text
+        bs_html         = BeautifulSoup(html, 'html.parser')
+        text            = bs_html.find_all('div', class_='showtxt')[0].text.replace('\xa0'*8,'\n\n')
+        return text
+
+    def write_file(self,path,name,text):
+        with open(path, 'a', encoding='utf-8') as f:
+            f.write(name + '\n')
+            f.writelines(text)
+            f.write('\n\n')
 
 if __name__ == "__main__":
-    target = 'https://www.biqukan.com/1_1094/5403177.html'
-    req = requests.get(url=target)
-    # 此网站采用gbk编码，因此需要对requests.get()获取的对象编码进行调整
-    # print(req.text.encode('iso-8859-1').decode('gbk'))    # 临时调整
-    req.encoding = 'gbk'                                    # 直接修改
-    html = req.text
-
-    bf = BeautifulSoup(html, 'html.parser')
-
-    texts = bf.find_all('div',id='content',class_='showtxt')
-    '''
-    find_all匹配的返回的结果是一个列表。
-    提取匹配结果后，使用text属性，提取文本内容，滤除br标签。
-    随后使用replace方法，剔除空格，替换为回车进行分段。 
-    在html中是用来表示空格的。replace(‘\xa0’*8,’\n\n’)就是去掉下图的八个空格符号，并用回车代替：
-    '''
-    texts = texts[0].text.replace('\xa0'*8,'\n\n')          
-    __output_File = open('output.txt','w')
-    __output_File.write(str(texts))
-    __output_File.close()
-
-    
+    dl = Download()
+    dl.get_novel()
+    print('开始下载:')
+    for index in range(dl.chapter_nums):
+        dl.write_file('一念永恒.txt',dl.chapter_name[index],dl.get_text(dl.chapter_url[index]))
+        sys.stdout.write('已下载:%.3f%%' % float(index / dl.chapter_nums) + '\r')
+        sys.stdout.flush()
+    print('下载结束！')
